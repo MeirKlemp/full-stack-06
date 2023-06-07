842dceb0e4dd999678158a6bf8d3d9153af8f9f6
Meir Klemfner <meirk251@outlook.com>
2023-06-07 13:52:13 +0300

    Init REST api project

---
+++ api/app.js
@@ -0,0 +1,30 @@
+import express from "express";
+import Response from "./util/response.js";
+import HttpStatus from "./util/http-status.js";
+
+const PORT = process.env.PORT || 2999;
+const app = express();
+app.use(express.json());
+
+console.log(HttpStatus);
+app.get("/", (req, res) =>
+  res.send(
+    new Response(
+      HttpStatus.OK.code,
+      HttpStatus.OK.status,
+      "Patient API, v1.0.0 - All Systems Go"
+    )
+  )
+);
+app.all("*", (req, res) => {
+  return res
+    .status(HttpStatus.NOT_FOUND.code)
+    .send(
+      new Response(
+        HttpStatus.NOT_FOUND.code,
+        HttpStatus.NOT_FOUND.status,
+        "Route does not exist on the server"
+      )
+    );
+});
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
