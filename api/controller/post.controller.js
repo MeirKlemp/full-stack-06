987e006807b4e660706fc369f5c686d46c41b9b6
אביחי סוריה <soria@g.jct.ac.il>
2023-06-07 23:25:38 +0300

    Finished initial CRUD maybe

--- api/controller/post.controller.js
+++ api/controller/post.controller.js
@@ -1,3 +1,5 @@
+import Joi from "joi";
+
 import database from "../config/mysql.config.js";
 import Response from "../util/response.js";
 import HttpStatus from "../util/http-status.js";
@@ -8,6 +10,13 @@ const isUserVerified = () => {
   // Probably need to get cookie apiKey as parameter
   return true;
 };
+
+// Expecting to get object of [userId, title, body]
+const createPostSchema = Joi.object({
+  userId: Joi.string().required(),
+  title: Joi.string().min(1).required(),
+  body: Joi.string().min(1).required(),
+});
 
 const handleUnauthorized = (res) => {
   res
@@ -33,21 +42,45 @@ const handleDatabaseError = (res) => {
     );
 };
 
+// Function to handle bad request errors
+const handleBadRequest = (res, message) => {
+  res
+    .status(HttpStatus.BAD_REQUEST.code)
+    .send(
+      new Response(
+        HttpStatus.BAD_REQUEST.code,
+        HttpStatus.BAD_REQUEST.status,
+        message
+      )
+    );
+};
+
+const handleNotFound = (res, message) => {
+  console.log(message);
+  res
+    .status(HttpStatus.NOT_FOUND.code)
+    .send(
+      new Response(
+        HttpStatus.NOT_FOUND.code,
+        HttpStatus.NOT_FOUND.status,
+        message
+      )
+    );
+};
+
 export const getPosts = (req, res) => {
   console.log(`${req.method} ${req.originalUrl}, fetching posts...`);
 
   // Check if the user is verified
   if (!isUserVerified()) {
-    handleUnauthorized(res);
-    return; // Exit the function after sending the response
+    return handleUnauthorized(res);
   }
 
   database.query(QUERY.SELECT_POSTS, (error, results) => {
     if (error) {
       // Handle the database error
       console.error("Error connecting to the database: ", error);
-      handleDatabaseError(res);
-      return; // Exit the function after sending the response
+      return handleDatabaseError(res);
     }
 
     if (!results) {
@@ -74,143 +107,143 @@ export const getPosts = (req, res) => {
     }
   });
 };
-/*
-export const createPatient = (req, res) => {
-  logger.info(`${req.method} ${req.originalUrl}, creating patient`);
-  database.query(
-    QUERY.CREATE_PATIENT_PROCEDURE,
-    Object.values(req.body),
-    (error, results) => {
-      if (!results) {
-        logger.error(error.message);
+
+export const createPost = (req, res) => {
+  console.log(`${req.method} ${req.originalUrl}, creating post`);
+
+  if (!isUserVerified()) {
+    return handleUnauthorized(res);
+  }
+
+  // Validate the request body against the defined schema
+  const { error, value } = createPostSchema.validate(req.body);
+
+  if (error) {
+    return handleBadRequest(res, error.details[0].message);
+  }
+
+  database.query(QUERY.CREATE_POST, Object.values(value), (error, results) => {
+    if (error) {
+      console.log(error.message);
+      return handleDatabaseError(res);
+    }
+
+    const post = results[0][0];
+    res
+      .status(HttpStatus.CREATED.code)
+      .send(
+        new Response(
+          HttpStatus.CREATED.code,
+          HttpStatus.CREATED.status,
+          `Post created`,
+          { post }
+        )
+      );
+  });
+};
+
+export const getPost = (req, res) => {
+  console.log(`${req.method} ${req.originalUrl}, fetching post`);
+
+  if (!isUserVerified()) {
+    return handleUnauthorized(res);
+  }
+
+  database.query(QUERY.SELECT_POST, [req.params.id], (error, results) => {
+    if (error) {
+      console.error("Error connecting to the database:", error);
+      return handleDatabaseError(res);
+    }
+
+    if (!results[0]) {
+      const message = `Post by id ${req.params.id} was not found`;
+      return handleNotFound(res, message);
+    }
+
+    res
+      .status(HttpStatus.OK.code)
+      .send(
+        new Response(
+          HttpStatus.OK.code,
+          HttpStatus.OK.status,
+          `Post retrieved`,
+          results[0]
+        )
+      );
+  });
+};
+
+export const updatePost = (req, res) => {
+  console.log(`${req.method} ${req.originalUrl}, fetching post`);
+
+  if (!isUserVerified()) {
+    return handleUnauthorized(res);
+  }
+
+  const { error } = postSchema.validate(req.body);
+  if (error) {
+    return handleBadRequest(res, error.details[0].message);
+  }
+
+  database.query(QUERY.SELECT_POST, [req.params.id], (error, results) => {
+    if (error) {
+      return handleDatabaseError(res, error);
+    }
+
+    if (!results[0]) {
+      return handleNotFound(res, `Post by id ${req.params.id} was not found`);
+    }
+
+    console.log(`${req.method} ${req.originalUrl}, updating post`);
+
+    database.query(
+      QUERY.UPDATE_POST,
+      [...Object.values(req.body), req.params.id],
+      (error) => {
+        if (error) {
+          return handleDatabaseError(res, error);
+        }
+
         res
-          .status(HttpStatus.INTERNAL_SERVER_ERROR.code)
+          .status(HttpStatus.OK.code)
           .send(
             new Response(
-              HttpStatus.INTERNAL_SERVER_ERROR.code,
-              HttpStatus.INTERNAL_SERVER_ERROR.status,
-              `Error occurred`
-            )
-          );
-      } else {
-        //const patient = { id: results.insertedId, ...req.body, created_at: new Date() };
-        const patient = results[0][0];
-        res
-          .status(HttpStatus.CREATED.code)
-          .send(
-            new Response(
-              HttpStatus.CREATED.code,
-              HttpStatus.CREATED.status,
-              `Patient created`,
-              { patient }
+              HttpStatus.OK.code,
+              HttpStatus.OK.status,
+              `Post updated`,
+              { id: req.params.id, ...req.body }
             )
           );
       }
-    }
-  );
-};
-
-export const getPatient = (req, res) => {
-  logger.info(`${req.method} ${req.originalUrl}, fetching patient`);
-  database.query(QUERY.SELECT_PATIENT, [req.params.id], (error, results) => {
-    if (!results[0]) {
-      res
-        .status(HttpStatus.NOT_FOUND.code)
-        .send(
-          new Response(
-            HttpStatus.NOT_FOUND.code,
-            HttpStatus.NOT_FOUND.status,
-            `Patient by id ${req.params.id} was not found`
-          )
-        );
-    } else {
-      res
-        .status(HttpStatus.OK.code)
-        .send(
-          new Response(
-            HttpStatus.OK.code,
-            HttpStatus.OK.status,
-            `Patient retrieved`,
-            results[0]
-          )
-        );
-    }
+    );
   });
 };
 
-export const updatePatient = (req, res) => {
-  logger.info(`${req.method} ${req.originalUrl}, fetching patient`);
-  database.query(QUERY.SELECT_PATIENT, [req.params.id], (error, results) => {
-    if (!results[0]) {
-      res
-        .status(HttpStatus.NOT_FOUND.code)
-        .send(
-          new Response(
-            HttpStatus.NOT_FOUND.code,
-            HttpStatus.NOT_FOUND.status,
-            `Patient by id ${req.params.id} was not found`
-          )
-        );
-    } else {
-      logger.info(`${req.method} ${req.originalUrl}, updating patient`);
-      database.query(
-        QUERY.UPDATE_PATIENT,
-        [...Object.values(req.body), req.params.id],
-        (error, results) => {
-          if (!error) {
-            res
-              .status(HttpStatus.OK.code)
-              .send(
-                new Response(
-                  HttpStatus.OK.code,
-                  HttpStatus.OK.status,
-                  `Patient updated`,
-                  { id: req.params.id, ...req.body }
-                )
-              );
-          } else {
-            logger.error(error.message);
-            res
-              .status(HttpStatus.INTERNAL_SERVER_ERROR.code)
-              .send(
-                new Response(
-                  HttpStatus.INTERNAL_SERVER_ERROR.code,
-                  HttpStatus.INTERNAL_SERVER_ERROR.status,
-                  `Error occurred`
-                )
-              );
-          }
-        }
+export const deletePost = (req, res) => {
+  console.log(`${req.method} ${req.originalUrl}, deleting post`);
+
+  if (!isUserVerified()) {
+    return handleUnauthorized(res);
+  }
+
+  database.query(QUERY.DELETE_POST, [req.params.id], (error, results) => {
+    if (error) {
+      return handleDatabaseError(res, error);
+    }
+
+    if (results.affectedRows === 0) {
+      return handleNotFound(res, `Post by id ${req.params.id} was not found`);
+    }
+
+    res
+      .status(HttpStatus.OK.code)
+      .send(
+        new Response(
+          HttpStatus.OK.code,
+          HttpStatus.OK.status,
+          `Post deleted`,
+          results[0]
+        )
       );
-    }
   });
 };
-
-export const deletePatient = (req, res) => {
-  logger.info(`${req.method} ${req.originalUrl}, deleting patient`);
-  database.query(QUERY.DELETE_PATIENT, [req.params.id], (error, results) => {
-    if (results.affectedRows > 0) {
-      res
-        .status(HttpStatus.OK.code)
-        .send(
-          new Response(
-            HttpStatus.OK.code,
-            HttpStatus.OK.status,
-            `Patient deleted`,
-            results[0]
-          )
-        );
-    } else {
-      res
-        .status(HttpStatus.NOT_FOUND.code)
-        .send(
-          new Response(
-            HttpStatus.NOT_FOUND.code,
-            HttpStatus.NOT_FOUND.status,
-            `Patient by id ${req.params.id} was not found`
-          )
-        );
-    }
-  });
-};
-*/
--- api/route/post.route.js
+++ api/route/post.route.js
@@ -1,24 +1,20 @@
 import express from "express";
-import { getPosts } from "../controller/post.controller.js";
-/*import {
+import {
   getPosts,
   createPost,
   getPost,
   deletePost,
   updatePost,
-} from "../controller/post.controller.js";*/
+} from "../controller/post.controller.js";
 
 const postRoutes = express.Router();
 
-//postRoutes.route("/").get(getPosts).post(createPost);
-postRoutes.route("/").get(getPosts);
+postRoutes.route("/").get(getPosts).post(createPost);
 
-/*
 postRoutes
   .route("/:id")
   .get(getPost)
   .put(updatePost)
   .delete(deletePost);
-  */
 
 export default postRoutes;
