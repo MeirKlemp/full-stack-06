8154f4569944f7c2bd9b4808f46f9493ff324f11
אביחי סוריה <soria@g.jct.ac.il>
2023-06-21 13:52:34 +0300

    Login

--- client/src/Pages/Login/Login.jsx
+++ client/src/Pages/Login/Login.jsx
@@ -7,28 +7,58 @@ function Login({ onLogin }) {
 
   async function handleSubmit(event) {
     event.preventDefault();
+    const user = { username, password };
     try {
-      const response = await fetch(
-        `https://jsonplaceholder.typicode.com/users?username=${username}`
-      );
-      const users = await response.json();
-      if (users.length === 0) {
-        alert("User was not found");
-        return;
-      }
-      const user = users[0];
-      const lat = user.address.geo.lat;
-      const actualPassword = lat.substring(lat.length - 4);
+      const response = await fetch("http://localhost:2999/apikeys", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(user),
+      });
 
-      if (password === actualPassword) {
+      if (response.ok) {
+        // Handle the successful response here
+        console.log("POST request successful");
+        const responseData = await response.json(); // Parse the response body as JSON
+        const apiKey = responseData.data.apiKey; // Retrieve the API key
+
+        // Construct the URL with the username parameter
+        const url = `http://localhost:2999/users/${username}`;
+
+        try {
+          const userResponse = await fetch(url, {
+            method: "GET",
+            headers: {
+              Authorization: `apikey ${apiKey}`,
+            },
+          });
+
+          if (userResponse.ok) {
+            const userJson = await userResponse.json();
+            setUser(userJson.data);
+            console.log("User data:", userJson.data);
+
+            // Navigate to the desired location
+            navigate("/");
+          } else {
+            console.error("Error fetching user:", userResponse.statusText);
+            // Handle the error
+          }
+        } catch (error) {
+          console.error("Error fetching user:", error);
+          // Handle the error
+        }
+
         alert("Login successful");
-        onLogin(user);
+        onLogin(username, apiKey);
       } else {
+        console.error("POST request failed");
         alert("Password is incorrect");
       }
     } catch (error) {
+      console.error("An error occurred:", error);
       alert("Error getting info from server");
-      console.error(error);
     }
   }
 
@@ -37,8 +67,11 @@ function Login({ onLogin }) {
       <h1>Login</h1>
       <form className="login-form" onSubmit={handleSubmit}>
         <div>
-          <label className="Login-Label" htmlFor="username">Username</label>
-          <input className="Login-Input"
+          <label className="Login-Label" htmlFor="username">
+            Username
+          </label>
+          <input
+            className="Login-Input"
             type="text"
             id="username"
             value={username}
@@ -46,8 +79,11 @@ function Login({ onLogin }) {
           />
         </div>
         <div>
-          <label className="Login-Label" htmlFor="password">Password</label>
-          <input className="Login-Input"
+          <label className="Login-Label" htmlFor="password">
+            Password
+          </label>
+          <input
+            className="Login-Input"
             type="password"
             id="password"
             value={password}
