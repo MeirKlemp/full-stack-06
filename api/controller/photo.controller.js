e0594a2592b137328adbba39a9baba9e64d0de80
אביחי סוריה <soria@g.jct.ac.il>
2023-06-21 09:14:11 +0300

    Tested and fixed photos's API, it should work properly now

--- api/controller/photo.controller.js
+++ api/controller/photo.controller.js
@@ -14,21 +14,27 @@ import generateQuery from "../query/queryUtils.js";
 const photoSchema = Joi.object({
   albumId: Joi.number().integer().required(),
   title: Joi.string().min(1).required(),
-  url: Joi.string().required(),
-  thumbnailUrl: Joi.string().required(),
+  url: Joi.string().uri().required(),
+  thumbnailUrl: Joi.string().uri().required(),
 });
 
 export const getPhotos = (req, res) => {
   console.log(`${req.method} ${req.originalUrl}, fetching photos...`);
 
-  const { limit, page } = req.query;
-  const { albumId } = req.params;
+  const { albumId, limit, page } = req.query;
+  const { userId } = res.locals;
 
   const conditions = [];
 
+  if (albumId) {
+    conditions.push(`albumId=${albumId}`);
+  }
+
+  conditions.push(`albumId IN (SELECT id FROM Albums WHERE userId=${userId})`);
+
   const query = generateQuery(QUERY.SELECT_PHOTOS, conditions, limit, page);
 
-  database.query(query, [albumId], (error, results) => {
+  database.query(query, [userId], (error, results) => {
     if (error) {
       console.error("Error getting photos:", error.message);
       return handleInternalError(res);
@@ -59,6 +65,36 @@ export const getPhotos = (req, res) => {
   });
 };
 
+export const getPhoto = (req, res) => {
+  console.log(`${req.method} ${req.originalUrl}, fetching photo`);
+  const { id } = req.params;
+  const { userId } = res.locals;
+  console.log(QUERY.SELECT_PHOTO, [id, userId]);
+
+  database.query(QUERY.SELECT_PHOTO, [id, userId], (error, results) => {
+    if (error) {
+      console.error("Error getting photo:", error.message);
+      return handleInternalError(res);
+    }
+
+    if (!results[0]) {
+      const message = `Photo with id ${id} was not found`;
+      return handleNotFound(res, message);
+    }
+
+    res
+      .status(HttpStatus.OK.code)
+      .send(
+        new Response(
+          HttpStatus.OK.code,
+          HttpStatus.OK.status,
+          "Photo retrieved",
+          results[0]
+        )
+      );
+  });
+};
+
 export const createPhoto = (req, res) => {
   console.log(`${req.method} ${req.originalUrl}, creating photo`);
 
@@ -69,10 +105,11 @@ export const createPhoto = (req, res) => {
   }
 
   const { albumId, title, url, thumbnailUrl } = req.body;
+  const { userId } = res.locals;
 
   database.query(
     QUERY.CREATE_PHOTO,
-    [albumId, title, url, thumbnailUrl],
+    [title, url, thumbnailUrl, albumId, userId],
     (error, results) => {
       if (error) {
         console.error("Error creating photo:", error.message);
@@ -103,35 +140,6 @@ export const createPhoto = (req, res) => {
   );
 };
 
-export const getPhoto = (req, res) => {
-  console.log(`${req.method} ${req.originalUrl}, fetching photo`);
-
-  const { albumId, id } = req.params;
-
-  database.query(QUERY.SELECT_PHOTO, [id, albumId], (error, results) => {
-    if (error) {
-      console.error("Error getting photo:", error.message);
-      return handleInternalError(res);
-    }
-
-    if (!results[0]) {
-      const message = `Photo by id ${id} was not found`;
-      return handleNotFound(res, message);
-    }
-
-    res
-      .status(HttpStatus.OK.code)
-      .send(
-        new Response(
-          HttpStatus.OK.code,
-          HttpStatus.OK.status,
-          "Photo retrieved",
-          results[0]
-        )
-      );
-  });
-};
-
 export const updatePhoto = (req, res) => {
   console.log(`${req.method} ${req.originalUrl}, updating photo`);
 
@@ -141,60 +149,57 @@ export const updatePhoto = (req, res) => {
     return handleBadRequest(res, error.details[0].message);
   }
 
-  const { albumId, id } = req.params;
+  const { id } = req.params;
   const { title, url, thumbnailUrl } = req.body;
+  const { userId } = res.locals;
 
-  database.query(QUERY.SELECT_PHOTO, [id, albumId], (error, results) => {
-    if (error) {
-      console.error("Error getting photo:", error.message);
-      return handleInternalError(res, error);
-    }
-
-    if (!results[0]) {
-      return handleNotFound(res, `Photo by id ${id} was not found`);
-    }
-
-    database.query(
-      QUERY.UPDATE_PHOTO,
-      [title, url, thumbnailUrl, id, albumId],
-      (error, results) => {
-        if (error) {
-          console.error("Error updating photo:", error.message);
-          return handleInternalError(res, error);
-        }
-
-        if (results.affectedRows === 0) {
-          return handleNotFound(res, `Photo by id ${id} was not found`);
-        }
-
-        res
-          .status(HttpStatus.OK.code)
-          .send(
-            new Response(
-              HttpStatus.OK.code,
-              HttpStatus.OK.status,
-              "Photo updated",
-              { id, albumId, title, url, thumbnailUrl }
-            )
-          );
+  database.query(
+    QUERY.UPDATE_PHOTO,
+    [title, url, thumbnailUrl, id, userId],
+    (error, results) => {
+      if (error) {
+        console.error("Error updating photo:", error.message);
+        return handleInternalError(res);
       }
-    );
-  });
+
+      if (results.affectedRows === 0) {
+        return handleNotFound(
+          res,
+          `Photo with id ${id} was not found or does not belong to the user`
+        );
+      }
+
+      res
+        .status(HttpStatus.OK.code)
+        .send(
+          new Response(
+            HttpStatus.OK.code,
+            HttpStatus.OK.status,
+            "Photo updated",
+            { id, albumId: null, ...req.body }
+          )
+        );
+    }
+  );
 };
 
 export const deletePhoto = (req, res) => {
   console.log(`${req.method} ${req.originalUrl}, deleting photo`);
 
-  const { albumId, id } = req.params;
+  const { id } = req.params;
+  const { userId } = res.locals;
 
-  database.query(QUERY.DELETE_PHOTO, [id, albumId], (error, results) => {
+  database.query(QUERY.DELETE_PHOTO, [id, userId], (error, results) => {
     if (error) {
       console.error("Error deleting photo:", error.message);
-      return handleInternalError(res, error);
+      return handleInternalError(res);
     }
 
     if (results.affectedRows === 0) {
-      return handleNotFound(res, `Photo by id ${id} was not found`);
+      return handleNotFound(
+        res,
+        `Photo with id ${id} was not found or does not belong to the user`
+      );
     }
 
     res
--- api/query/photo.query.js
+++ api/query/photo.query.js
@@ -1,11 +1,14 @@
 const QUERY = {
-  SELECT_PHOTOS: "SELECT * FROM Photos WHERE albumId = ?",
-  SELECT_PHOTO: "SELECT * FROM Photos WHERE id = ? AND albumId = ?",
+  SELECT_PHOTOS:
+    "SELECT * FROM Photos",
+  SELECT_PHOTO:
+    "SELECT * FROM Photos WHERE id = ? AND albumId IN (SELECT id FROM Albums WHERE userId = ?)",
   CREATE_PHOTO:
-    "INSERT INTO Photos(albumId, title, url, thumbnailUrl) VALUES (?, ?, ?, ?)",
+    "INSERT INTO Photos(albumId, title, url, thumbnailUrl) SELECT a.id, ?, ?, ? FROM Albums a WHERE a.id = ? AND a.userId = ?",
   UPDATE_PHOTO:
-    "UPDATE Photos SET title = ?, url = ?, thumbnailUrl = ? WHERE id = ? AND albumId = ?",
-  DELETE_PHOTO: "DELETE FROM Photos WHERE id = ? AND albumId = ?",
+    "UPDATE Photos SET title = ?, url = ?, thumbnailUrl = ? WHERE id = ? AND albumId IN (SELECT id FROM Albums WHERE userId = ?)",
+  DELETE_PHOTO:
+    "DELETE FROM Photos WHERE id = ? AND albumId IN (SELECT id FROM Albums WHERE userId = ?)",
 };
 
 export default QUERY;
