2077cdcbfcd6851169e961df7f49cb4b7b894c3c
אביחי סוריה <soria@g.jct.ac.il>
2023-06-21 19:10:03 +0300

    Getting emails and usernames from comments

--- api/query/comment.query.js
+++ api/query/comment.query.js
@@ -1,6 +1,6 @@
 const QUERY = {
-  SELECT_COMMENTS: "SELECT * FROM Comments",
-  SELECT_COMMENT: "SELECT * FROM Comments WHERE id = ?",
+  SELECT_COMMENTS: "SELECT Comments.*, Users.username, Users.email FROM Comments INNER JOIN Users ON Comments.userId = Users.id",
+  SELECT_COMMENT: "SELECT Comments.*, Users.username, Users.email FROM Comments INNER JOIN Users ON Comments.userId = Users.id WHERE Comments.id = ?",
   CREATE_COMMENT: "INSERT INTO Comments(userId, postId, body) VALUES (?, ?, ?)",
   UPDATE_COMMENT:
     "UPDATE Comments SET postId = ?, body = ? WHERE id = ? AND userId = ?",
